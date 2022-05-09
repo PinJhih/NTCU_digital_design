b630f5bcf6e7d0b124a52c33522deec71ccd1002
PinJhih <isaacwang29@gmail.com>
2022-05-09 18:44:09 +0800

    改善寫法

--- src/bcd_to_ex3/Top_sim.v
+++ src/bcd_to_ex3/Top_sim.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 10ps
 module Top();
 
-    reg bcd[3:0];
-    wire ex3[3:0];
+    reg [3:0]bcd;
+    wire [3:0]ex3;
 
     initial begin
         bcd[3] = 1'b0;
@@ -57,6 +57,7 @@ module Top();
         #25 $finish;
     end
 
-    bcd_to_ex3 encoder({bcd[3],bcd[2],bcd[1],bcd[0]}, {ex3[3],ex3[2],ex3[1],ex3[0]});
+    bcd_to_ex3 encoder(bcd, ex3);
+    // bcd_to_ex3 encoder({bcd[3],bcd[2],bcd[1],bcd[0]}, {ex3[3],ex3[2],ex3[1],ex3[0]}); 也可以
 
 endmodule
