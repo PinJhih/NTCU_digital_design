bd5bb7683e4c1892a0abf29e9801de7a8264cd00
PinJhih <isaacwang29@gmail.com>
2022-05-09 18:00:36 +0800

    調整順序

--- src/bcd_to_ex3/Top_design.v
+++ src/bcd_to_ex3/Top_design.v
@@ -1,8 +1,7 @@
 `timescale 1ns / 10ps
 module Top(
     input [3:0]sw,
-    output [3:0]led
-);
+    output [3:0]led);
 
     sw_to_ex3 encoder(sw, led);
 
--- src/bcd_to_ex3/Top_sim.v
+++ src/bcd_to_ex3/Top_sim.v
@@ -5,56 +5,56 @@ module Top();
     wire ex3[3:0];
 
     initial begin
-            bcd[0] = 1'b0;
-            bcd[1] = 1'b0;
-            bcd[2] = 1'b0;
-            bcd[3] = 1'b0;
-        #50
-            bcd[0] = 1'b0;
-            bcd[1] = 1'b0;
-            bcd[2] = 1'b0;
-            bcd[3] = 1'b1;
-        #50
-            bcd[0] = 1'b0;
-            bcd[1] = 1'b0;
-            bcd[2] = 1'b1;
-            bcd[3] = 1'b0;
-        #50
-            bcd[0] = 1'b0;
-            bcd[1] = 1'b0;
-            bcd[2] = 1'b1;
-            bcd[3] = 1'b1;
-        #50
-            bcd[0] = 1'b0;
-            bcd[1] = 1'b1;
-            bcd[2] = 1'b0;
-            bcd[3] = 1'b0;
-        #50
-            bcd[0] = 1'b0;
-            bcd[1] = 1'b1;
-            bcd[2] = 1'b0;
-            bcd[3] = 1'b1;
-        #50
-            bcd[0] = 1'b0;
-            bcd[1] = 1'b1;
-            bcd[2] = 1'b1;
-            bcd[3] = 1'b0;
-        #50
-            bcd[0] = 1'b0;
-            bcd[1] = 1'b1;
-            bcd[2] = 1'b1;
-            bcd[3] = 1'b1;
-        #50
-            bcd[0] = 1'b1;
-            bcd[1] = 1'b0;
-            bcd[2] = 1'b0;
-            bcd[3] = 1'b0;
-        #50
-            bcd[0] = 1'b1;
-            bcd[1] = 1'b0;
-            bcd[2] = 1'b0;
-            bcd[3] = 1'b1;
-        #50 $finish;
+        bcd[3] = 1'b0;
+        bcd[2] = 1'b0;
+        bcd[1] = 1'b0;
+        bcd[0] = 1'b0;
+        #25
+        bcd[3] = 1'b0;
+        bcd[2] = 1'b0;
+        bcd[1] = 1'b0;
+        bcd[0] = 1'b1;
+        #25
+        bcd[3] = 1'b0;
+        bcd[2] = 1'b0;
+        bcd[1] = 1'b1;
+        bcd[0] = 1'b0;
+        #25
+        bcd[3] = 1'b0;
+        bcd[2] = 1'b0;
+        bcd[1] = 1'b1;
+        bcd[0] = 1'b1;
+        #25
+        bcd[3] = 1'b0;
+        bcd[2] = 1'b1;
+        bcd[1] = 1'b0;
+        bcd[0] = 1'b0;
+        #25
+        bcd[3] = 1'b0;
+        bcd[2] = 1'b1;
+        bcd[1] = 1'b0;
+        bcd[0] = 1'b1;
+        #25
+        bcd[3] = 1'b0;
+        bcd[2] = 1'b1;
+        bcd[1] = 1'b1;
+        bcd[0] = 1'b0;
+        #25
+        bcd[3] = 1'b0;
+        bcd[2] = 1'b1;
+        bcd[1] = 1'b1;
+        bcd[0] = 1'b1;
+        #25
+        bcd[3] = 1'b1;
+        bcd[2] = 1'b0;
+        bcd[1] = 1'b0;
+        bcd[0] = 1'b0;
+        #25
+        bcd[3] = 1'b1;
+        bcd[2] = 1'b0;
+        bcd[1] = 1'b0;
+        bcd[0] = 1'b1;
+        #25 $finish;
     end
 
     bcd_to_ex3 encoder({bcd[3],bcd[2],bcd[1],bcd[0]}, {ex3[3],ex3[2],ex3[1],ex3[0]});
