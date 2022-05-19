1046c2c754bbab87ce639780e6fbb52275c2a0c6
PinJhih <isaacwang29@gmail.com>
2022-05-19 17:01:42 +0800

    移除用於硬體平台的檔案

--- src/bcd_to_ex3/Top_design.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 10ps
-module Top(
-    input [3:0]sw,
-    output [3:0]led);
-
-    sw_to_ex3 encoder(sw, led);
-
-endmodule
--- src/bin_adder/Top_design.v
+++
@@ -1,8 +0,0 @@
-module Top(
-    input [3:0]sw,
-    input [3:0]dip_pin,
-    output [5:0]led
-);
-    bin_adder adder(num1,num2, sum);
-
-endmodule
